94e87554e93dd8b1044dbd23916a54181089ba29
System <none>
2017-06-26 19:48:43 +0000

    Checkpoint

--- skills/test.js
+++ skills/test.js
@@ -2,6 +2,23 @@ module.exports = function(telegram_controller, slack_controller) {
   
   telegram_controller.on('message_received', function(bot, message) {
     bot.reply(message, 'Hello')
+    bot.reply(message, 'Hello from Jon')
+    
+    
+  })
+  
+  telegram_controller.hears('^hi$', 'message_received', function(bot, message) {
+    
+    bot.startConversation(message, function(err, convo) {
+      convo.addMessage('Hi there', 'default')
+      
+      convo.addQuestion('What is your favorite color?', function(convo, res) {
+        convo.say('Your favorite color is ', res.text)
+        convo.next()
+      })
+    
+    convo.next()
+    })
   })
   
 }
