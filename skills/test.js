ffab3d94f9459b32bd4e01aae8a16ab98d700196
System <none>
2017-06-28 09:59:16 +0000

    Checkpoint

--- skills/test.js
+++ skills/test.js
@@ -2,7 +2,7 @@ module.exports = function(telegram_controller, slack_controller) {
   
   telegram_controller.on('message_received', function(bot, message) {
     bot.reply(message, 'Hello')
-    bot.reply(message, 'Hello from Jon')
+    bot.reply(message, 'Hello from Sam')
     
     
   })
