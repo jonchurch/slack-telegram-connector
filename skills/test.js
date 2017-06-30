cfcb436ca089f7f350458e82b4e37ad4395c61f5
System <none>
2017-06-30 02:16:28 +0000

    Checkpoint

--- skills/test.js
+++ skills/test.js
@@ -7,16 +7,16 @@ module.exports = function(telegram, slack) {
   var telegramBot = telegram.spawn({})
  telegram.on('message_received', function(bot, message) {
    console.log(message)
-   var name = message.profile.fn + message.profile.ln
-   name = name.toLowerCase() 
+   var name = message.profile.fn + ' ' + message.profile.ln
+   // name = name.toLowerCase() 
    var slack_message = {
      // text: '',
       "attachments": [
         {
             "fallback": "Required plain-text summary of the attachment.",
             
-            // "author_name": message.profile.fn + message.profile.ln,
-            "title": name,
+            "author_name": message.profile.fn + message.profile.ln,
+            // "title": name,
             text: message.text,
         }
     ]
